c5db85ef406417726fbd3430512d52e9b01fdd43
LiuJianwen219 <jianwen_liu219@163.com>
2021-09-28 21:15:01 +0800

    feat: class

---
+++ Class/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+
+from Class import views
+
+urlpatterns = [
+    path('create_class/', views.create_class),
+]
+
--- Class/views.py
+++ Class/views.py
@@ -1,3 +1,29 @@
+import json
+from datetime import datetime
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from Class.models import TheClass
+from Course.models import Course, CourseTemplate
+
 
 # Create your views here.
+def create_class(request):
+    print(request.POST)
+    print(request.POST["courseId"])
+    print(request.POST["templateId"])
+    print(request.POST["newClassName"])
+    print(request.POST["newClassStartTime"])
+    print(request.POST["newClassEndTime"])
+
+    course = Course.objects.get(uid=request.POST["courseId"])
+    courseTemplate = CourseTemplate.objects.get(uid=request.POST["templateId"])
+
+    start = datetime.strptime(request.POST["newClassStartTime"], '%Y-%m-%d')
+    end = datetime.strptime(request.POST["newClassEndTime"], '%Y-%m-%d')
+    the_class = TheClass(course=course, course_template=courseTemplate, name=request.POST["newClassName"],
+                         start_date=start, end_date=end)
+    the_class.save()
+
+    data = {"state": "OK"}
+    return HttpResponse(json.dumps(data), content_type='application/json')

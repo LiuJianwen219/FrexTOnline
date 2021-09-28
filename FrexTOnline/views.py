1c2b0123fae4013b05bbba1d9c77ae62aabeca2c
LiuJianwen219 <jianwen_liu219@163.com>
2021-09-28 18:18:38 +0800

    feat: course

--- FrexTOnline/views.py
+++ FrexTOnline/views.py
@@ -1,43 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+from Login.models import User
+from Course.models import Course
 
 # Create your views here.
 def test(request):
-    freeExperiment = {
-        'expItems': [
-            {
-                'expId': "0001",
-                'expName': "exp01",
-                'fileList': [
-                    {
-                        'fileId': "fid0001",
-                        'fileName': "wenjian01"
-                    },
-                    {
-                        'fileId': "fid0002",
-                        'fileName': "wenjian02"
-                    },
-                ]
-            },
-            {
-                'expId': "0002",
-                'expName': "exp02",
-                'fileList': [
-                    {
-                        'fileId': "fid0002",
-                        'fileName': "wenjian02"
-                    },
-                    {
-                        'fileId': "fid0003",
-                        'fileName': "wenjian03"
-                    },
-                ]
-            },
-        ]
-
-    }
-    return render(request, 'Home/P1.html', {'freeExperiment': freeExperiment, 'text': "text"})
+    user = User.objects.get(request.session.get('u_uid'))
+    c = Course(user=user, name="数字逻辑")
+    c.save()
+    return render(request, 'Home/home.html')
 
 
 def favicon(request):

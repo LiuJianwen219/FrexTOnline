31996a0180265fd3778a68ae168d56d1d80bcc29
LiuJianwen219 <jianwen_liu219@163.com>
2021-09-28 18:19:54 +0800

    feat: course

--- FrexTOnline/views.py
+++ FrexTOnline/views.py
@@ -6,7 +6,7 @@ from Course.models import Course
 
 # Create your views here.
 def test(request):
-    user = User.objects.get(request.session.get('u_uid'))
+    user = User.objects.get(uid=request.session["u_uid"])
     c = Course(user=user, name="数字逻辑")
     c.save()
     return render(request, 'Home/home.html')
